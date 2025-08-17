ca113c729e480739951d98957c708a4504b3f71f
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2025-08-16 21:24:37 -0700

    trying to foce DO for rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";                     // layout (no router inside)
+import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Admin from "./pages/Admin.jsx";
 import ServicesPage from "./pages/ServicesPage.jsx";
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        {/* Public pages share the App layout good practice */}
+        {/* Public pages share the App layout */}
         <Route element={<App />}>
           <Route index element={<Home />} />
           <Route path="services" element={<ServicesPage />} />
